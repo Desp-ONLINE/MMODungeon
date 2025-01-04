cc8200e703eeb4c8b868d54fd6949b65ccfbbd78
nao505 <nao505@naver.com>
2025-01-04 17:52:16 +0900

    Add MMODungeonAPI and MainConfig classes for core functionality
    Introduced `MMODungeonAPI` to provide access to player and dungeon repositories and `MainConfig` to handle configuration with MongoDB integration. These changes lay the foundation for managing plugin settings and repository access in a centralized manner.

---
+++ src/main/java/com/binggre/mmodungeon/api/events/MMODungeonAPI.java
@@ -0,0 +1,16 @@
+package com.binggre.mmodungeon.api.events;
+
+import com.binggre.mmodungeon.MMODungeon;
+import com.binggre.mmodungeon.repository.DungeonRepository;
+import com.binggre.mmodungeon.repository.PlayerRepository;
+
+public class MMODungeonAPI {
+
+    public static PlayerRepository getPlayerRepository() {
+        return MMODungeon.getPlugin().getPlayerRepository();
+    }
+
+    public static DungeonRepository getDungeonRepository() {
+        return MMODungeon.getPlugin().getDungeonRepository();
+    }
+}
---
+++ src/main/java/com/binggre/mmodungeon/config/MainConfig.java
@@ -0,0 +1,39 @@
+package com.binggre.mmodungeon.config;
+
+import com.binggre.binggreapi.utils.file.FileManager;
+import com.binggre.mmodungeon.MMODungeon;
+import com.binggre.mongolibraryplugin.MongoLibraryPlugin;
+import com.mongodb.client.MongoCollection;
+import lombok.Getter;
+import org.bson.Document;
+
+import java.util.Set;
+
+@Getter
+public class MainConfig {
+
+    public static String COLLECTION = "MainConfig";
+    private static MainConfig instance = null;
+
+    public static MainConfig getInstance() {
+        if (instance == null) {
+            instance = new MainConfig();
+        }
+        return instance;
+    }
+
+    private Set<String> allowCommands;
+
+    public void init() {
+        MongoCollection<Document> collection = MongoLibraryPlugin.getInst().getMongoClient()
+                .getDatabase(MMODungeon.DATA_BASE_NAME)
+                .getCollection(COLLECTION);
+
+        Document configDocument = collection.find().first();
+        if (configDocument == null) {
+            configDocument = new Document();
+            configDocument.append("allowCommands", Set.of("dungeon"));
+        }
+        instance = FileManager.toObject(configDocument.toJson(), MainConfig.class);
+    }
+}
